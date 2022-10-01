b929482b1e008cb113066e0e727db46114597d60
JavGog <javierggjavier@gmail.com>
2022-10-01 16:43:41 -0700

    New Login file functionality passed from App file.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
 import './App.css';
 import './background-image.jpg'
+import Login from './Login';
 
 function App() {
-  return (
-    <div className="App">
-      <div class="full-screen-container">
-    <div class="login-container">
-      <h1 class="login-title">Welcome</h1>
-      <form class="form">
-        <div class="input-group success">
-          <label for="email">Email</label>
-          <input type="email" name="email" id="email"/>
-          <span class="msg">Valid email</span>
-        </div>
-
-        <div class="input-group error">
-          <label for="password">Password</label>
-          <input type="password" name="password" id="password"/>
-          <span class="msg">Incorrect password</span>
-        </div>
-
-        <button type="submit" class="login-button">Login</button>
-      </form>
-    </div>
-  </div>
-    </div>
+  return (<Login/>
   );
 }
 
